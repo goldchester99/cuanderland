983ebd971a0279bea903f73c6234ae9dd89e3e46
Yakobus Obie <obie.arch@gmail.com>
2026-04-30 18:28:49 +0700

    02

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("Pondasi Terminal Trading")
+import streamlit as st
+
+st.set_page_config(page_title="Cuanderland Terminal", layout="wide")
+
+st.title("🏗️ Cuanderland Trading Terminal")
+st.write("Selamat datang di Markas Besar, Bung Arsitek!")
+st.success("Sistem Online 100%. Menunggu modul Screener & Watchlist...")
